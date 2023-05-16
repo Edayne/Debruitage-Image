5fc8c33bebec66f9203c67f27609c6c81f731f9b
Saïf-Eddine KEHILI <kehilisaif@cy-tech.fr>
2023-05-16 14:00:45 +0200

    Affichage de l'image initiale

--- src/Photo.java
+++ src/Photo.java
@@ -3,6 +3,10 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.io.File;
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 
 public class Photo {
@@ -14,12 +18,37 @@ public class Photo {
     public Photo() {
         this.photo = null;
         try {
-            this.photo = ImageIO.read(new File("donnees/lena.jpg"));
+            this.photo = ImageIO.read(new File("../donnees/lena.png"));
         } catch (IOException e) {
         }
     }
 
     //Methodes
+    /**
+     * @return BufferedImage return the photo
+     */
+    public BufferedImage getPhoto() {
+        return photo;
+    }
 
+    /**
+     * @param photo the photo to set
+     */
+    public void setPhoto(BufferedImage photo) {
+        this.photo = photo;
+    }
+
+    //Main
+    public static void main(String[] args) {
+        Photo image = new Photo();
+        JFrame frame = new JFrame();
+        frame.getContentPane().setLayout(new FlowLayout());
+        frame.getContentPane().add(new JLabel(new ImageIcon(image.photo)));
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    
 
 }
