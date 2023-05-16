de704ff33b9c35cac737dabeccd61de88cfe00d4
Saïf-Eddine KEHILI <kehilisaif@cy-tech.fr>
2023-05-16 10:47:21 +0200

    tentative de lecture d'image

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
-//salut
+import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class Main {
-
-	public static void main(String[] args) {
+	BufferedImage image = new BufferedImage(null, null, false, null);
+	
+	//ImageIO.read(PerformanceTest.class.getResource("donnees/lena.jpg"));
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		//uygug
-ahdfjia	fhiaf
+		
 	}
-
+		BufferedImage hugeImage = ImageIO.read(PerformanceTest.class.getResource("donnees/lena.jpg"));
 }
-gftrgg
-//jeanmichjel
