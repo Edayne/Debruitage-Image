37dec36f3896a0ed986cbb399391fe098e2e99ce
Laura-drdle <darridolel@cy-tech.fr>
2023-05-16 15:48:32 +0200

    Création class et fonction seuillagedoux

--- src/SeuillageDur.java
+++ src/SeuillageDur.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class SeuillageDur{
 
     public ArrayList<Double> seuillageDur(double seuil, ArrayList<Double> coefficients){
-        ArrayList<Double> resultat = new ArrayList<Double>(); //créer tableau résultat de même taille que coefficients et qui va stocker le résultat du seuillage dur
+        ArrayList<Double> resultat = new ArrayList<Double>(); //Créer tableau résultat de même taille que coefficients et qui va stocker le résultat du seuillage dur
         int n = coefficients.size();
         
         for (int i = 0; i < n; i++){ // On parcours tous les itérés du tableau 
