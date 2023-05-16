a4af41c1e3040edbc790f6fc402a8ee0b8748319
Saïf-Eddine KEHILI <kehilisaif@cy-tech.fr>
2023-05-16 10:26:43 +0200

    2-3 trucs

--- src/Image.java
+++ src/Image.java
@@ -1,6 +1,10 @@
-
+import java.util.ArrayList;
 public class Image {
 
+	public ArrayList<Image> ExtractPatchs(Image X, int s) {
+
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
