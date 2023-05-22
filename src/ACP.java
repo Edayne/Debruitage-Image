824224f415ec973a1bbd1422308c9d051bfac458
ulrichnk <kapkapulrich@gmail.com>
2023-05-22 11:00:27 +0200

    m

--- src/ACP.java
+++ src/ACP.java
@@ -43,7 +43,7 @@ public class ACP {
         if (variance-(s*s) <=0){
             return(0);
         }else{
-            return(Math.sqrt(variance-(s*s)));
+            return((s*s)/Math.sqrt(variance-(s*s)));
         }
     }
     
